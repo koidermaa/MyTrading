4f5c3ff29f77ce2d8d17001886af9de3a47386bf
koidermaa <oidermaa@gmail.com>
2015-12-25 17:41:58 +0200

    mõlemad arvutused töötavad, fx kood

--- src/mytrading/Arvutused.java
+++ src/mytrading/Arvutused.java
@@ -1,5 +1,13 @@
 package mytrading;
 
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Arvutused extends MainData {
@@ -46,12 +54,18 @@ public class Arvutused extends MainData {
             }
 
         }
-        UserInterface user = new UserInterface();
-        user.populate(buyTickers, buyPrices, sellTickers, sellPrices);
+       String bollinger = new String ("latest price");
+       populate(buyTickers, buyPrices, sellTickers, sellPrices, bollinger);
 
     }
 
     public void strategy2(double [][]histPrices){
+
+        ArrayList<String> buyTickers = new ArrayList<String>();
+        ArrayList<Double> rsiLevelsBuy = new ArrayList<Double>();
+        ArrayList<String> sellTickers = new ArrayList<String>();
+        ArrayList<Double> rsiLevelsSell = new ArrayList<Double>();
+
         for (int j = 0; j <tickers.length ; j++) {
             double summa3=0, summa4=0;
             symbol = tickers[j];
@@ -67,13 +81,96 @@ public class Arvutused extends MainData {
                 }
             }
             double RSI = 100 - (100/(1-summa3/summa4));
-            //System.out.println(symbol + " RSI on " + RSI+"  "+ summa3 + "  "+summa4);
+
             if (RSI< 40) {
-                System.out.println("osta "+symbol +" RSI on ju "+ RSI);
+
+                buyTickers.add(symbol);
+                rsiLevelsBuy.add(RSI);
+
             } else if (RSI> 60) {
-                System.out.println("müü "+symbol +" RSI on ju " + RSI);
+
+                sellTickers.add(symbol);
+                rsiLevelsSell.add(RSI);
             }
         }
+        String rsi = new String ("rsi");
+        populate(buyTickers, rsiLevelsBuy, sellTickers, rsiLevelsSell, rsi);
+
     }
+
+    public void populate(ArrayList<String> buyTickers,ArrayList<Double> buyPrices,ArrayList<String> sellTickers,ArrayList<Double> sellPrices, String dataType) {
+        int buySize = buyTickers.size();
+        int sellSize = sellTickers.size();
+        int i =0;
+
+        Stage stage = new Stage();
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(10,10,10,10));
+        pane.setHgap(10);
+        pane.setVgap(1);
+        pane.setStyle("-fx-background-color: #F8F8F8;");
+        DecimalFormat df = new DecimalFormat("####0.00");
+
+        if (dataType.equals("rsi")){
+            stage.setTitle("RSI");
+        } else if (dataType.equals("latest price")){
+            stage.setTitle("Bollinger");
+        }
+
+        Text results = new Text("Results:");
+        pane.add(results,0,0);
+
+
+        if (buySize> 0){
+
+            String sentence = "Buy the following stocks ("+ dataType +"):";
+            Text buy = new Text(sentence);
+            pane.add(buy,0,i+2,5,1);
+            for (i = 0; i < buySize; i++) {
+
+                String aktsia = buyTickers.get(i);
+                Hyperlink link = new Hyperlink(aktsia);
+                link.setOnAction(event -> {
+                    new StockData(aktsia);
+                });
+                pane.add(link, 0, i + 3);
+                double price = buyPrices.get(i);
+                String price2 = df.format(price);
+                Text hind = new Text(price2);
+                pane.add(hind, 1, i+3);
+            }
+        }  else {
+            Text buy = new Text("No stocks to buy!!");
+            pane.add(buy,0,i+2,3,1);
+        }
+
+        if (sellSize> 0){
+            Text sell = new Text("Sell the following stocks ("+dataType+"):");
+            pane.add(sell,0,i+5,5,1);
+
+            for (int j = 0; j < sellSize; j++) {
+
+                String aktsia = sellTickers.get(i);
+                Hyperlink link = new Hyperlink(aktsia);
+                link.setOnAction(event -> {
+                    new StockData(aktsia);
+                });
+                pane.add(link, 0, i + 6+j);
+                double price = sellPrices.get(i);
+                String price2 = df.format(price);
+                Text hind = new Text(price2);
+                pane.add(hind, 1, i+6+j);
+            }
+        }  else {
+            Text sell = new Text("No stocks to sell!!");
+            pane.add(sell, 0, i + 5, 3, 1);
+        }
+        Scene stseen = new Scene(pane,500,500);
+        stage.setScene(stseen);
+
+        stage.show();
+
+    }
+
 
 }
