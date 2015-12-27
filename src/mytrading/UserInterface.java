02c7d99b8e7d1e899033cf54d333914bbb6ced58
koidermaa <oidermaa@gmail.com>
2015-12-27 21:57:50 +0200

    notice lisatud

--- src/mytrading/UserInterface.java
+++ src/mytrading/UserInterface.java
@@ -5,7 +5,10 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -22,6 +25,7 @@ public class UserInterface {
 
         layout.setTitle("MyTrading 2.0");
         GridPane pane = addGrid1();
+        border.setStyle("-fx-background-color: #F8F8F8;");
         border.setTop(pane);
 
         Scene stseen = new Scene(border,300,200);
@@ -36,14 +40,14 @@ public class UserInterface {
         pane.setPadding(new Insets(10,10,10,10));
         pane.setHgap(10);
         pane.setVgap(5);
+        pane.setStyle("-fx-background-color: #F8F8F8;");
         MainData maindata = new MainData();
 
-        Button logOut = new Button("Log out");
-        pane.add(logOut, 3, 1);
         Button getData = new Button("Get data from IB");
         pane.add(getData, 1, 1, 2,1);
         getData.setOnAction(event -> {
-            maindata.getData();
+            double time = maindata.getData();
+            notice(time);
         });
 
         Button strategy1 = new Button("Bollinger");
@@ -69,6 +73,18 @@ public class UserInterface {
         });
 
         return pane;
+    }
+
+    public void notice(double time){
+
+        StackPane pane = new StackPane();
+        pane.setStyle("-fx-background-color: #F8F8F8;");
+        String tekst="Process finished in "+time+" seconds";
+        Text text = new Text(tekst);
+        text.setFont(Font.font("Verdana", 12));
+        text.setFill(Color.RED);
+        pane.getChildren().add(text);
+        border.setCenter(pane);
     }
 
 
